d35136337ded65338b42117c944da2cbfb10377e
AzenkChina <azenk1991@163.com>
2019-12-04 14:44:46 +0800

    learning

--- STM32F091/src/main.rs
+++ STM32F091/src/main.rs
@@ -9,63 +9,25 @@ extern crate panic_halt;
 extern crate cortex_m_semihosting;
 
 use cortex_m_rt::{entry, exception};
-use stm32f0::stm32f0x1;
+use stm32f0::stm32f0x1::{interrupt};
 #[cfg(debug_assertions)]
 use cortex_m_semihosting::hprintln;
 
 
 #[link(name = "devices", kind = "static")]
 extern {
+	fn SVC_Handler();
+	fn PendSV_Handler();
+	fn SysTick_Handler();
+	fn RTC_IRQHandler();
+	fn USART3_8_IRQHandler();
+	fn USART1_IRQHandler();
 	fn udelay(v: u16);
 	fn mdelay(v: u16);
 }
 
-
-/// Set up RCC
-fn rcc_init(peripherals: &mut stm32f0x1::Peripherals) {
-}
-
-/// Set up the systick to provide a 100ms timebase
-fn systick_init(syst: &mut stm32f0x1::SYST) {
-    syst.set_reload((8_000_000 / 8) / 10);
-    syst.clear_current();
-    syst.set_clock_source(cortex_m::peripheral::syst::SystClkSource::External);
-    syst.enable_interrupt();
-    syst.enable_counter();
-}
-
-/// Set up gpio
-fn gpio_init(peripherals: &mut stm32f0x1::Peripherals) {
-    let gpioa = &peripherals.GPIOA;
-
-    //LED
-    gpioa.moder.modify(|_, w| w.moder5().output());
-    gpioa.odr.modify(|_, w| w.odr5().clear_bit());
-}
-
-///
-fn led_positive(peripherals: &mut stm32f0x1::Peripherals) {
-    let gpioa = &peripherals.GPIOA;
-
-    gpioa.odr.modify(|_, w| w.odr5().set_bit());
-}
-
-///
-fn led_negative(peripherals: &mut stm32f0x1::Peripherals) {
-    let gpioa = &peripherals.GPIOA;
-
-    gpioa.odr.modify(|_, w| w.odr5().clear_bit());
-}
-
 #[entry]
 fn main() -> ! {
-    let mut peripherals = stm32f0x1::Peripherals::take().unwrap();
-    let mut core = stm32f0x1::CorePeripherals::take().unwrap();
-
-    rcc_init(&mut peripherals);
-	gpio_init(&mut peripherals);
-    systick_init(&mut core.SYST);
-
 #[cfg(debug_assertions)]
 	hprintln!("Call C function.").unwrap();
 	unsafe{ udelay(100); }
@@ -78,15 +40,17 @@ fn main() -> ! {
 	hprintln!("Hello, World!").unwrap();
 
     loop {
-		cortex_m::asm::wfi();
-		led_positive(&mut peripherals);
-		cortex_m::asm::wfi();
-		led_negative(&mut peripherals);
+		unsafe{ mdelay(100); }
     }
 }
 
 #[exception]
-fn SysTick() {
+fn DefaultHandler(irqn: i16) {
+	panic!("Unhandled exception (IRQn = {})", irqn);
+}
+
+#[exception]
+fn NonMaskableInt() {
 
 }
 
@@ -96,6 +60,31 @@ fn HardFault(ef: &cortex_m_rt::ExceptionFrame) -> ! {
 }
 
 #[exception]
-fn DefaultHandler(irqn: i16) {
-	panic!("Unhandled exception (IRQn = {})", irqn);
+fn SVCall() {
+	unsafe{ SVC_Handler(); }
+}
+
+#[exception]
+fn PendSV() {
+	unsafe{ PendSV_Handler(); }
+}
+
+#[exception]
+fn SysTick() {
+	unsafe{ SysTick_Handler(); }
+}
+
+#[interrupt]
+fn RTC() {
+	unsafe{ RTC_IRQHandler(); }
+}
+
+#[interrupt]
+fn USART1() {
+	unsafe{ USART1_IRQHandler(); }
+}
+
+#[interrupt]
+fn USART3_4_5_6_7_8() {
+	unsafe{ USART3_8_IRQHandler(); }
 }
